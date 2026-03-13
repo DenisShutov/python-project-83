cd8c64bcc438f9c967de58579f1769cf8e0b6229
DenisShutov <shtvdenis93@yandex.ru>
2026-03-13 21:37:05 +0500

    add get_url_checks, save_check, change get_content

--- url_repository.py
+++ url_repository.py
@@ -19,7 +19,14 @@ class UrlRepository:
     def get_content(self):
         with self.get_connection() as conn:
             with conn.cursor(cursor_factory=RealDictCursor) as cur:
-                cur.execute("SELECT * FROM urls;")
+                sql = """
+                SELECT u.*, MAX(u_c.created_at) AS last_check
+                FROM urls as u
+                LEFT JOIN url_checks as u_c ON u.id = u_c.url_id
+                GROUP BY u.id, u.name, u.created_at
+                ORDER BY u.created_at DESC
+                """
+                cur.execute(sql)
                 return cur.fetchall()
             
 #получение данных об url по id    
@@ -42,7 +49,8 @@ class UrlRepository:
                 result = cur.fetchone()
                 conn.commit()
                 return result['id']
-    
+
+#поиск id по имени(url)    
     def find_by_name(self, name):
         with self.get_connection() as conn:
             with conn.cursor(cursor_factory=RealDictCursor) as cur:
@@ -51,4 +59,27 @@ class UrlRepository:
                 """
                 cur.execute(sql, (name,))
                 return cur.fetchone()
-               
+    
+
+    def save_check(self, url_id):
+        with self.get_connection() as conn:
+            with conn.cursor(cursor_factory=RealDictCursor) as cur:
+                sql = """
+                INSERT INTO url_checks (url_id)
+                VALUES (%s) RETURNING id;
+                """
+                cur.execute(sql, (url_id,))
+                check_id = cur.fetchone()
+                conn.commit()
+                return check_id
+    
+    def get_url_checks(self, url_id):
+        with self.get_connection() as conn:
+            with conn.cursor(cursor_factory=RealDictCursor) as cur:
+                sql = """
+                SELECT * FROM url_checks WHERE url_id = %s
+                ORDER BY created_at DESC;
+                """
+                cur.execute(sql, (url_id,))
+                return cur.fetchall()
+    
